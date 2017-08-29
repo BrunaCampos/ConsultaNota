868cd59b72052b6331ccb51b4da34d9583fed3df
bruna <bruna.sc1997@hotmail.com>
2017-08-28 23:38:05 -0300

    First Commit

---
+++ SistemaNotas/SistemaNotas/Entities/Account/ApplicationUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaNotas.Entities
+{
+    public class ApplicationUser : IdentityUser<long>
+    {
+    }
+}
