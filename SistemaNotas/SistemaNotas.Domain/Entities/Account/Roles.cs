a4363bae48a4826ff4c92d6e8295326091e854c0
bruna <bruna.sc1997@hotmail.com>
2017-08-29 20:57:05 -0300

    reference domain

--- SistemaNotas/SistemaNotas.Domain/Entities/Account/Roles.cs
+++ SistemaNotas/SistemaNotas.Domain/Entities/Account/Roles.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace ConsultaNotas.Entities.Account
+namespace ConsultaNotas.Domain.Entities.Account
 {
     public class Roles : IdentityRole<long>
     {
